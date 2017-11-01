fe4ef397e8e288f0bf955dd4239215552af60777
Lincoln Luiz <lincolnluiz.com@gmail.com>
2017-11-01 11:53:29 -0300

    Comentários adicionados;

--- src/main/java/com/github/lincolnluiz/SpringJasper/controller/RelatorioController.java
+++ src/main/java/com/github/lincolnluiz/SpringJasper/controller/RelatorioController.java
@@ -32,15 +32,24 @@ public class RelatorioController {
 
 	@PostMapping
 	public void imprimir(@RequestParam Map<String, Object> parametros, HttpServletResponse response) throws JRException, SQLException, IOException {
-		parametros = parametros != null ? parametros = new HashMap<>() : parametros;
 		
+		parametros = parametros == null ? parametros = new HashMap<>() : parametros;
+		
+		// Pega o arquivo .jasper localizado em resources
 		InputStream jasperStream = this.getClass().getResourceAsStream("/relatorios/livros.jasper");
+		
+		// Cria o objeto JaperReport com o Stream do arquivo jasper
 		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+		// Passa para o JasperPrint o relatório, os parâmetros e a fonte dos dados, no caso uma conexão ao banco de dados
 		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource.getConnection());
 
+		// Configura a respota para o tipo PDF
 		response.setContentType("application/pdf");
+		// Define que o arquivo pode ser visualizado no navegador e também nome final do arquivo
+		// para fazer download do relatório troque 'inline' por 'attachment'
 		response.setHeader("Content-Disposition", "inline; filename=livros.pdf");
 
+		// Faz a exportação do relatório para o HttpServletResponse
 		final OutputStream outStream = response.getOutputStream();
 		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
 	}
