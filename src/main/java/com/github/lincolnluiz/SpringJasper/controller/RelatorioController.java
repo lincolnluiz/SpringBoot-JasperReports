10b3da46decb20ba63edd7b1455f3fd063e25728
andervilo <andervilo@hotmail.com>
2019-11-27 09:45:03 -0300

    ArrayList como DataSource

--- src/main/java/com/github/lincolnluiz/SpringJasper/controller/RelatorioController.java
+++ src/main/java/com/github/lincolnluiz/SpringJasper/controller/RelatorioController.java
@@ -16,11 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.github.lincolnluiz.SpringJasper.model.LivroRepository;
+
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 
 @Controller
@@ -29,6 +32,9 @@ public class RelatorioController {
 	
 	@Autowired
 	private DataSource dataSource;
+	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	@PostMapping
 	public void imprimir(@RequestParam Map<String, Object> parametros, HttpServletResponse response) throws JRException, SQLException, IOException {
@@ -42,6 +48,31 @@ public class RelatorioController {
 		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
 		// Passa para o JasperPrint o relatório, os parâmetros e a fonte dos dados, no caso uma conexão ao banco de dados
 		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource.getConnection());
+
+		// Configura a respota para o tipo PDF
+		response.setContentType("application/pdf");
+		// Define que o arquivo pode ser visualizado no navegador e também nome final do arquivo
+		// para fazer download do relatório troque 'inline' por 'attachment'
+		response.setHeader("Content-Disposition", "inline; filename=livros.pdf");
+
+		// Faz a exportação do relatório para o HttpServletResponse
+		final OutputStream outStream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+	}
+	
+	@PostMapping("/jrds")
+	public void imprimirTwo(@RequestParam Map<String, Object> parametros, HttpServletResponse response) throws JRException, SQLException, IOException {
+		
+		parametros = parametros == null ? parametros = new HashMap<>() : parametros;
+		JRBeanCollectionDataSource beanBurritoWrap = new JRBeanCollectionDataSource(livroRepository.findAll());
+		
+		// Pega o arquivo .jasper localizado em resources
+		InputStream jasperStream = this.getClass().getResourceAsStream("/relatorios/livros2.jasper");
+		
+		// Cria o objeto JaperReport com o Stream do arquivo jasper
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+		// Passa para o JasperPrint o relatório, os parâmetros e a fonte dos dados, no caso uma conexão ao banco de dados
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, beanBurritoWrap);
 
 		// Configura a respota para o tipo PDF
 		response.setContentType("application/pdf");
---
+++ src/main/java/com/github/lincolnluiz/SpringJasper/model/Livro.java
@@ -0,0 +1,49 @@
+package com.github.lincolnluiz.SpringJasper.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "livro")
+public class Livro {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	@Column(name = "id_livro")
+	protected Long id;
+	
+	protected String autor;
+
+	protected String titulo;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+
+}
